8ade3cc3f55f54896084bb751f7b17aed18fcb29
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-04-23 17:25:07 +0300

    Added Tab Layout With ViewPager2

--- app/src/main/java/com/example/taisacco/TaiSacco.kt
+++ app/src/main/java/com/example/taisacco/TaiSacco.kt
@@ -6,26 +6,76 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.viewpager2.widget.ViewPager2
+import com.example.taisacco.databinding.FragmentTaiSaccoBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.taisacco.Enquiry
+import com.example.taisacco.Loans
+import com.example.taisacco.Transactions
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class TaiSacco : Fragment() {
+    private lateinit var binding: FragmentTaiSaccoBinding
+    private lateinit var demoCollectionAdapter: DemoCollectionAdapter
+    private lateinit var viewPager: ViewPager2
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tai_sacco, container, false)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_tai_sacco,
+            container,
+            false
+        )
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        demoCollectionAdapter = DemoCollectionAdapter(this)
+        viewPager = view.findViewById(R.id.viewPager)
+        viewPager.adapter = demoCollectionAdapter
 
+        tabLayout =view.findViewById(R.id.tabs)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when(position) {
+                0 -> "Enquiry"
+                1->"Transactions"
+                3-> "Loans"
+                else-> "About"
+            }
+        }.attach()
+
+    }
+
+}
+
+class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+
+        return when(position) {
+            0 -> {
+                Enquiry()
+            }
+            1 -> {
+                Transactions()
+            }
+            3-> {
+                Loans()
+            }
+            else -> {
+                Enquiry()
+            }
+        }
+    }
 }
