2fc9ca66778b33375674484e3ad0ede7b7c9f8cb
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-04-23 16:54:46 +0300

    Added Navigation Drawer

--- app/src/main/java/com/example/taisacco/MainActivity.kt
+++ app/src/main/java/com/example/taisacco/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.taisacco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.taisacco.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
+    lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+        drawerLayout = binding.drawer
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
