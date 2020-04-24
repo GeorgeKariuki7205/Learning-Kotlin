333e1cab40591c341ccaeab4978c23835280d0f0
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-04-24 14:16:49 +0300

    Added the click functionality in the cardViews

--- app/src/main/java/com/example/taisacco/Loans.kt
+++ app/src/main/java/com/example/taisacco/Loans.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.taisacco.databinding.FragmentLoansBinding
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,13 +21,34 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class Loans : Fragment() {
-
+    private lateinit var binding: FragmentLoansBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_loans, container, false)
+//        return inflater.inflate(R.layout.fragment_loans, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_loans,
+            container,
+            false
+        )
+        binding.card18.setOnClickListener {
+            Toast.makeText(context, "Withdraw Cash Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.card19.setOnClickListener {
+            Toast.makeText(context, "Inter Account Transfer Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.card20.setOnClickListener {
+            Toast.makeText(context, "Deposit Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.cards21.setOnClickListener {
+            Toast.makeText(context, "Buy Airtime Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/taisacco/Transactions.kt
+++ app/src/main/java/com/example/taisacco/Transactions.kt
@@ -6,25 +6,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.taisacco.databinding.FragmentTransactionsBinding
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class Transactions : Fragment() {
-
+    private lateinit var binding: FragmentTransactionsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_transactions, container, false)
+//        return inflater.inflate(R.layout.fragment_transactions, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_transactions,
+            container,
+            false
+        )
+        binding.card14.setOnClickListener {
+            Toast.makeText(context, "Withdraw Cash Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.card15.setOnClickListener {
+            Toast.makeText(context, "Inter Account Transfer Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.card16.setOnClickListener {
+            Toast.makeText(context, "Deposit Clicked", Toast.LENGTH_SHORT).show()
+        }
+        binding.cards17.setOnClickListener {
+            Toast.makeText(context, "Buy Airtime Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return binding.root
     }
 
 
