6951e8f262061f1b4109df98eee0731d1d3ee30e
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-05-07 11:35:02 -0700

    Implemented the safearg to the application

--- app/src/main/java/com/example/taisacco/Reciever.kt
+++ app/src/main/java/com/example/taisacco/Reciever.kt
@@ -5,18 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.taisacco.databinding.FragmentRecieverBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class Reciever : Fragment() {
-
+    private lateinit var binding: FragmentRecieverBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reciever, container, false)
+//        return inflater.inflate(R.layout.fragment_reciever, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_reciever,
+            container,
+            false
+        )
+
+        var args = RecieverArgs.fromBundle(arguments!!)
+        Toast.makeText(context, "NumCorrect: ${args.name}, NumQuestions: ${args.name}", Toast.LENGTH_LONG).show()
+        binding.textView4.text = args.name
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/taisacco/UpdateProfile.kt
+++ app/src/main/java/com/example/taisacco/UpdateProfile.kt
@@ -5,18 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.taisacco.databinding.FragmentUpdateProfileBinding
+import com.example.taisacco.UpdateProfileDirections
 /**
  * A simple [Fragment] subclass.
  */
 class UpdateProfile : Fragment() {
-
+    private lateinit var binding: FragmentUpdateProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_profile, container, false)
+//        return inflater.inflate(R.layout.fragment_update_profile, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_update_profile,
+            container,
+            false
+        )
+
+
+        binding.submit.setOnClickListener {
+            var name = binding.value.text
+            findNavController().navigate(UpdateProfileDirections.actionUpdateProfileToReciever(name.toString()))
+//            Toast.makeText(context, "NumCorrect: ${name}, NumQuestions: ${name}", Toast.LENGTH_LONG).show()
+        }
+
+        return  binding.root
     }
 
 }
